099ecbe1a87b0ed649ce69a1251ef9f2b3ab84ba
kankanla <koyi_s@hotmail.com>
2017-11-16 01:40:05 +0900

    Bluetooth test

---
+++ app/src/main/java/com/kankanla/e560/m1104a/work/Bluetooth_status.java
@@ -0,0 +1,19 @@
+package com.kankanla.e560.m1104a.work;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Bluetooth_status extends Service {
+
+    public Bluetooth_status() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+}
